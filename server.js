0fe1b859b2bd3962a44ea1a7c6324e6cfd3e90c5
ashish jksol <cubes.dev2@gmail.com>
2024-05-27 14:28:49 +0530

    chnage the code

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.get('/api/get_user_details',(req,res) => {
 let users = [
     { id: 1, name: 'John Doe', email: 'john@example.com' },
     { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 3, name: 'John Doe', email: 'john@example.com' },
+    { id: 4, name: 'John Doe', email: 'john@example.com' },
+    { id: 5, name: 'John Doe', email: 'john@example.com' },
+    { id: 6, name: 'John Doe', email: 'john@example.com' },
+    { id: 7, name: 'John Doe', email: 'john@example.com' },
+    { id: 8, name: 'John Doe', email: 'john@example.com' },
+    { id: 9, name: 'John Doe', email: 'john@example.com' },
+    { id: 10, name: 'John Doe', email: 'john@example.com' },
+    { id: 11, name: 'John Doe', email: 'john@example.com' },
+
 ];
 
 // Routes
